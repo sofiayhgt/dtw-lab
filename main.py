c8d58c01796e95fc736bca02d77c87611726778e
isallorcag <202100739@alu.comillas.edu>
2026-02-06 18:33:37 +0100

    yes

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ if __name__=='__main__':
  df = read_csv_from_google_drive('1eKiAZKbWTnrcGs3bqdhINo1E4rBBpglo')
  df = clean_data(df)
  visualize_data(df)
- print(f'The mean value for the charge left 20 is {calculate_statistic('mean',df['Charge_Left_Percentage'])}')
+ print(f'The mean value for the charge left 20 is {calculate_statistic('mode',df['Charge_Left_Percentage'])}')
